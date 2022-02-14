a474017fd530d925c7c37caf9fb792523fc13824
Nagarajuvuliki <87876269+Nagarajuvuliki@users.noreply.github.com>
2022-02-14 07:26:34 +0530

    Update login_flask.py

--- login_flask.py
+++ login_flask.py
@@ -1,11 +1,12 @@
 import re
+from datetime import timedelta
 from dowellconnection1 import dowellconnection
-from multiprocessing import context
-from flask import Flask, Response, redirect, url_for, request, session, g, render_template
+from flask import Flask, redirect, url_for, request, session, g, render_template
 from aessecurity import AEScipher
 from login_fun import dowelllogin
 import os
 app = Flask(__name__)
+
 app.secret_key = os.urandom(24)
 @app.route('/',methods =["GET","POST"])
 def login():
@@ -20,10 +21,11 @@ def login():
             # voice=request.files['voice']
             # voice.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(voice.filename)))
             # lang=form["language"]
-            # loc=form["loc"]
-            # device=form["dev"]
-            # osver=form["os"]
-            # conn=form["conn"]
+            loc=form["loc"]
+            device=form["dev"]
+            osver=form["os"]
+            brow=form["brow"]
+            ltime=form["time"]
             pwd1=AEScipher(pwd)
             userid=dowelllogin(user,pwd1.encrypt())
             if "User Not Found" in userid:
@@ -33,8 +35,9 @@ def login():
                 session['loggedin'] = True
                 session['id'] = userid[1]
                 session['username'] = userid[0]
-                msg="this only readable page"
-                return render_template('profile.html', msg = msg)
+                field={"Username":userid[0],"OS":osver,"Device":device,"Browser":brow,"Location":loc,"Time":ltime,"SessionID":userid[1]}
+                dowellconnection("login","bangalore","login","login","login","6752828281","ABCDE","insert",field,"nil")
+                return render_template('profile.html')
     return render_template("home.html",context=context)
 @app.route('/logout')
 def logout():
@@ -56,7 +59,7 @@ def register():
         user=form["user"]
         field={"Username":user}
         login=dowellconnection("login","bangalore","login","registration","registration","10004545","ABCDE","fetch",field,"nil")
-        
+
         pwd=form["pass"]
         first=form["first"]
         last=form["last"]
@@ -72,7 +75,7 @@ def register():
             return render_template("test.html",context=context)
         elif email in login:
             context["error"]="Email already exist"
-            return render_template("test.html",context=context)    
+            return render_template("test.html",context=context)
         if re.fullmatch(r'[A-Za-z0-9@#$%^&+=]{8,}', pwd):
             pwd1=AEScipher(pwd)
             try:
@@ -86,12 +89,16 @@ def register():
                 return render_template("test.html",context=context)
         else:
             context["error"]="password must be alpanumeric and special charecter"
-            return render_template("test.html",context=context)       
+            return render_template("test.html",context=context)
 @app.before_request
 def before_request():
     g.user=None
     if 'username' in session:
         g.user=session["username"]
+@app.before_request
+# def make_session_permanent():
+#     session.permanent = True
+#     app.permanent_session_lifetime = timedelta(minutes=1)
 
 if __name__=='__main__':
     app.run(debug=True,ssl_context='adhoc')
